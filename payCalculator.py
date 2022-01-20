a9d7a02c500481056f4b0f8187ecb781c1a03382
Emma West <Emma.West@vu.com>
2022-01-20 14:45:22 -0600

    fix print

--- payCalculator.py
+++ payCalculator.py
@@ -5,7 +5,7 @@ def calculatePay():
     hrs = input("Enter Hours: ")
     rate = input("Enter Rate: ")
     pay = hrs*rate
-    Print("Pay:", pay)
+    print("Pay:", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
